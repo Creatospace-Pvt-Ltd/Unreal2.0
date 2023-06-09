d76133cad428422e7b59b17c076e8456ed95a7a0
Saswat Mishra <saswatmishrabablu@gmail.com>
2023-06-09 13:50:48 +0530

    Convai and metahuman

---
+++ Plugins/Convai/Source/Convai/Convai.Build.cs
@@ -0,0 +1,227 @@
+// Copyright 2022 Convai Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+// using Tools.DotNETCommon;
+
+
+
+public class Convai : ModuleRules
+{
+    private static ConvaiPlatform ConvaiPlatformInstance;
+
+    private string ModulePath
+	{
+		get { return ModuleDirectory; }
+	}
+
+	private string ThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModulePath, "../ThirdParty/")); }
+	}
+
+    // private bool IsUE426
+    // {
+    //     get
+    //     {
+    //         //var VersionFilePath = Path.Combine(UnrealBuildTool.EngineDirectory.FullName, "Build" + Path.DirectorySeparatorChar + "Build.version");
+
+    //         FileReference BuildVersionFile = BuildVersion.GetDefaultFileName();
+
+    //         BuildVersion Version;
+    //         if (BuildVersion.TryRead(BuildVersionFile, out Version))
+    //         {
+    //             if (Version.MajorVersion == 4 && Version.MinorVersion == 26)
+    //             {
+    //                 return true;
+    //             }
+    //         }
+    //         return false;
+    //     }
+    // }
+
+    private ConvaiPlatform GetConvaiPlatformInstance(ReadOnlyTargetRules Target)
+    {
+        var ConvaiPlatformType = System.Type.GetType("ConvaiPlatform_" + Target.Platform.ToString());
+        if (ConvaiPlatformType == null)
+        {
+            throw new BuildException("Convai does not support platform " + Target.Platform.ToString());
+        }
+
+        var PlatformInstance = Activator.CreateInstance(ConvaiPlatformType) as ConvaiPlatform;
+        if (PlatformInstance == null)
+        {
+            throw new BuildException("Convai could not instantiate platform " + Target.Platform.ToString());
+        }
+
+        return PlatformInstance;
+    }
+
+    private bool ConfigurePlatform(ReadOnlyTargetRules Target, UnrealTargetConfiguration Configuration)
+    {
+        //Convai thirdparty libraries root path
+        string root = ThirdPartyPath;
+        foreach (var arch in ConvaiPlatformInstance.Architectures())
+        {
+            string grpcPath = root + "grpc/" + "lib/" + ConvaiPlatformInstance.LibrariesPath + arch;
+            PublicAdditionalLibraries.AddRange(Directory.GetFiles(grpcPath));
+            //if (Target.Platform == UnrealTargetPlatform.Android)
+            //{
+                //string openSSLPath = root + "OpenSSL/" + "lib/" + ConvaiPlatformInstance.LibrariesPath + arch;
+                //PublicAdditionalLibraries.AddRange(Directory.GetFiles(openSSLPath));
+                //PrivateIncludePaths.AddRange(
+                //    new string[] {
+                //    Path.Combine(ThirdPartyPath, "OpenSSL", "Include"),
+                //    });
+            //}
+            //foreach (var a in PublicAdditionalLibraries)
+            //    Console.WriteLine(a);
+        }
+        return false;
+    }
+
+    public Convai(ReadOnlyTargetRules Target) : base(Target)
+	{
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        ConvaiPlatformInstance = GetConvaiPlatformInstance(Target);
+        bUsePrecompiled = true;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", 
+			"CoreUObject",
+			"Engine",
+			"InputCore" ,
+			"HTTP", 
+			"Json",
+			"JsonUtilities",        
+			"AudioMixer",
+			"AudioCaptureCore",
+			"AudioCapture",
+			"Voice",
+            "SignalProcessing",
+			"libOpus",
+            "OpenSSL",
+            "zlib"
+        });
+
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PublicDependencyModuleNames.AddRange(new string[] { "ApplicationCore", "AndroidPermission" });
+            string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(BuildPath, "Convai_AndroidAPL.xml"));
+        }
+
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Opus codec thirdparty dependency
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "libOpus");
+
+        AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
+        AddEngineThirdPartyPrivateStaticDependencies(Target, "zlib");
+
+
+
+        //PublicDefinitions.Add("PROTOBUF_INLINE_NOT_IN_HEADERS=0");
+        //PublicDefinitions.Add("GPR_FORBID_UNREACHABLE_CODE=0");
+        PublicDefinitions.Add("ConvaiDebugMode=1");
+
+        PublicDefinitions.Add("GOOGLE_PROTOBUF_NO_RTTI");
+        PublicDefinitions.Add("GPR_FORBID_UNREACHABLE_CODE");
+        PublicDefinitions.Add("GRPC_ALLOW_EXCEPTIONS=0");
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+					Path.Combine(ThirdPartyPath, "gRPC", "Include"),
+					//Path.Combine(ThirdPartyPath, "OpenSSL\\1.1.1k\\include\\Android\\openssl"),
+			});
+
+
+
+        //if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+			PrecompileForTargets = PrecompileTargetsType.Any;
+		}
+
+        //ThirdParty Libraries
+        ConfigurePlatform(Target, Target.Configuration);
+    }
+}
+
+public abstract class ConvaiPlatform
+{
+    public virtual string ConfigurationDir(UnrealTargetConfiguration Configuration)
+    {
+        if (Configuration == UnrealTargetConfiguration.Debug || Configuration == UnrealTargetConfiguration.DebugGame)
+        {
+            return "Debug/";
+        }
+        else
+        {
+            return "Release/";
+        }
+    }
+    public abstract string LibrariesPath { get; }
+    public abstract List<string> Architectures();
+    public abstract string LibraryPrefixName { get; }
+    public abstract string LibraryPostfixName { get; }
+}
+
+public class ConvaiPlatform_Win64 : ConvaiPlatform
+{
+    public override string LibrariesPath { get { return "win64/"; } }
+    public override List<string> Architectures() { return new List<string> { "" }; }
+    public override string LibraryPrefixName { get { return ""; } }
+    public override string LibraryPostfixName { get { return ".lib"; } }
+}
+
+public class ConvaiPlatform_Android : ConvaiPlatform
+{
+    public override string LibrariesPath { get { return "android/"; } }
+    public override List<string> Architectures() { return new List<string> { "armeabi-v7a/", "arm64-v8a/" }; }
+    public override string LibraryPrefixName { get { return "lib"; } }
+    public override string LibraryPostfixName { get { return ".a"; } }
+}
+
+//public class ConvaiPlatform_Linux : ConvaiPlatform
+//{
+//    public override string LibrariesPath { get { return "linux/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_PS5 : ConvaiPlatform
+//{
+//    public override string LibrariesPath { get { return "ps5/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_Mac : ConvaiPlatform
+//{
+//    public override string ConfigurationDir(UnrealTargetConfiguration Configuration)
+//    {
+//        return "";
+//    }
+//    public override string LibrariesPath { get { return "mac/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_IOS : ConvaiPlatform
+//{
+//    public override string ConfigurationDir(UnrealTargetConfiguration Configuration)
+//    {
+//        return "";
+//    }
+//    public override string LibrariesPath { get { return "ios/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
---
+++ Plugins/Convai/Source/Convai/Private/ConvaiSubsystem.cpp
@@ -0,0 +1,239 @@
+// Copyright 2022 Convai Inc. All Rights Reserved.
+
+#include "ConvaiSubsystem.h"
+#include "ConvaiAndroid.h"
+
+THIRD_PARTY_INCLUDES_START
+// grpc includes
+#include <grpc++/grpc++.h>
+#include <chrono>
+THIRD_PARTY_INCLUDES_END
+
+
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+using service::ConvaiService;
+
+DEFINE_LOG_CATEGORY(ConvaiSubsystemLog);
+
+using grpc::SslCredentialsOptions;
+
+#if PLATFORM_WINDOWS
+#include <wincrypt.h>
+namespace
+{
+
+	std::string utf8Encode(const std::wstring& wstr)
+	{
+		if (wstr.empty())
+			return std::string();
+
+		int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(),
+			NULL, 0, NULL, NULL);
+		std::string strTo(sizeNeeded, 0);
+		WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(),
+			&strTo[0], sizeNeeded, NULL, NULL);
+		return strTo;
+	}
+
+	SslCredentialsOptions getSslOptions()
+	{
+		// Fetch root certificate as required on Windows (s. issue 25533).
+		SslCredentialsOptions result;
+
+		// Open root certificate store.
+		HANDLE hRootCertStore = CertOpenSystemStoreW(NULL, L"ROOT");
+		if (!hRootCertStore)
+			return result;
+
+		// Get all root certificates.
+		PCCERT_CONTEXT pCert = NULL;
+		while ((pCert = CertEnumCertificatesInStore(hRootCertStore, pCert)) != NULL)
+		{
+			// Append this certificate in PEM formatted data.
+			DWORD size = 0;
+			CryptBinaryToStringW(pCert->pbCertEncoded, pCert->cbCertEncoded,
+				CRYPT_STRING_BASE64HEADER, NULL, &size);
+			std::vector<WCHAR> pem(size);
+			CryptBinaryToStringW(pCert->pbCertEncoded, pCert->cbCertEncoded,
+				CRYPT_STRING_BASE64HEADER, pem.data(), &size);
+
+			result.pem_root_certs += utf8Encode(pem.data());
+		}
+
+		CertCloseStore(hRootCertStore, 0);
+		return result;
+	}
+};
+#endif
+
+namespace
+{
+	const char* grpc_connectivity_state_str[] =
+	{
+		"GRPC_CHANNEL_IDLE",
+		"GRPC_CHANNEL_CONNECTING",
+		"GRPC_CHANNEL_READY",
+		"GRPC_CHANNEL_TRANSIENT_FAILURE",
+		"GRPC_CHANNEL_SHUTDOWN"
+	};
+
+};
+
+
+
+uint32 FgRPCClient::Run()
+{
+    void* got_tag;
+    bool ok = false;
+	UE_LOG(ConvaiSubsystemLog, Log, TEXT("Start Run"));
+
+    // Block until the next result is available in the completion queue "cq
+	while (bIsRunning && cq_.Next(&got_tag, &ok)) {
+		if (got_tag)
+		{
+			FgRPC_Delegate* gRPC_Delegate = static_cast<FgRPC_Delegate*>(got_tag);
+			AsyncTask(ENamedThreads::GameThread, [gRPC_Delegate, ok] {gRPC_Delegate->ExecuteIfBound(ok); });
+		}
+		else
+		{
+			UE_LOG(ConvaiSubsystemLog, Log, TEXT("Bad got_tag"));
+		}
+		//FPlatformProcess::Sleep(.5);
+    }
+	UE_LOG(ConvaiSubsystemLog, Log, TEXT("End Run"));
+
+	return 0;
+}
+
+void FgRPCClient::StartStub()
+{
+	OnStateChangeDelegate = FgRPC_Delegate::CreateRaw(this, &FgRPCClient::OnStateChange);
+	CreateChannel();
+
+	bIsRunning = true;
+	Thread.Reset(FRunnableThread::Create(this, TEXT("gRPC_Stub")));
+}
+
+void FgRPCClient::CreateChannel()
+{
+	UE_LOG(ConvaiSubsystemLog, Log, TEXT("gRPC Creating Channel..."));
+	Channel = grpc::CreateChannel(Target, Creds);
+	
+	//Channel->NotifyOnStateChange(grpc_connectivity_state::GRPC_CHANNEL_CONNECTING, std::chrono::system_clock::time_point().max(), &cq_, (void*)&OnStateChangeDelegate);
+}
+
+void FgRPCClient::OnStateChange(bool ok)
+{
+	//if (!bIsRunning)
+	//	return;
+
+	grpc_connectivity_state state;
+	
+	if (Channel)
+		state = Channel->GetState(true);
+	else
+		state = grpc_connectivity_state::GRPC_CHANNEL_SHUTDOWN;
+
+
+	if (state == grpc_connectivity_state::GRPC_CHANNEL_SHUTDOWN)
+	{
+		if (!bIsRunning)
+		{
+			UE_LOG(ConvaiSubsystemLog, Warning, TEXT("gRPC channel state changed to %s... Attempting to reconnect"), *FString(grpc_connectivity_state_str[state]));
+			CreateChannel();
+		}
+		else
+		{
+			UE_LOG(ConvaiSubsystemLog, Log, TEXT("gRPC channel state changed to %s... Closing"), *FString(grpc_connectivity_state_str[state]));
+		}
+		//Channel->NotifyOnStateChange(state, std::chrono::system_clock::time_point().max(), &cq_, (void*)&OnStateChangeDelegate);
+	}
+	else
+	{
+		UE_LOG(ConvaiSubsystemLog, Log, TEXT("gRPC channel state changed to %s"), *FString(grpc_connectivity_state_str[state]));
+		Channel->NotifyOnStateChange(state, std::chrono::system_clock::time_point().max(), &cq_, (void*)&OnStateChangeDelegate);
+	}
+}
+
+void FgRPCClient::Exit()
+{
+	//Channel.reset();
+	//Channel = nullptr;
+
+	if (!bIsRunning)
+	{
+		return;
+	}
+
+	bIsRunning = false;
+	{
+		FScopeLock Lock(&CriticalSection);
+		cq_.Shutdown();
+	}
+	//Thread->WaitForCompletion();
+	//Thread->Kill();
+}
+
+FgRPCClient::FgRPCClient(std::string InTarget,
+	const std::shared_ptr<grpc::ChannelCredentials>& InCreds)
+	: bIsRunning(false),
+	Target(InTarget),
+	Creds(InCreds)
+{
+}
+
+std::unique_ptr<ConvaiService::Stub> FgRPCClient::GetNewStub()
+{
+	grpc_connectivity_state state = Channel->GetState(false);
+
+	if (state != grpc_connectivity_state::GRPC_CHANNEL_READY)
+	{
+		UE_LOG(ConvaiSubsystemLog, Warning, TEXT("gRPC channel not ready yet.. Current State: %s"), *FString(grpc_connectivity_state_str[state]));
+		//return nullptr;
+	}
+	return ConvaiService::NewStub(Channel);
+}
+
+CompletionQueue* FgRPCClient::GetCompletionQueue()
+{
+	return &cq_;
+}
+
+UConvaiSubsystem::UConvaiSubsystem()
+	: UGameInstanceSubsystem()
+{
+}
+
+void UConvaiSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+#if PLATFORM_WINDOWS
+	auto channel_creds = grpc::SslCredentials(getSslOptions());
+#elif PLATFORM_ANDROID
+	auto channel_creds = grpc::SslCredentials(grpc::SslCredentialsOptions());
+#endif
+	gRPC_Runnable = MakeShareable(new FgRPCClient(std::string("stream.convai.com"), channel_creds));
+
+	gRPC_Runnable->StartStub();
+	UE_LOG(ConvaiSubsystemLog, Log, TEXT("UConvaiSubsystem Started"));
+
+	GetAndroidMicPermission();
+}
+
+void UConvaiSubsystem::Deinitialize()
+{
+	gRPC_Runnable->Exit();
+	Super::Deinitialize();
+	UE_LOG(ConvaiSubsystemLog, Log, TEXT("UConvaiSubsystem Stopped"));
+}
+
+void UConvaiSubsystem::GetAndroidMicPermission()
+{
+	if (!UConvaiAndroid::ConvaiAndroidHasMicrophonePermission())
+		UConvaiAndroid::ConvaiAndroidAskMicrophonePermission();
+}
