1b03ad237ee87d9fa388b8a68010557cceb9f113
Saswat Mishra <saswatmishrabablu@gmail.com>
2023-12-16 03:23:00 +0530

    screenshare Android fix

--- Build/Android/src/com/metaverse/creatospace/OBBData.java
+++ Build/Android/src/com/metaverse/creatospace/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"5", // the version of the APK that the file was uploaded against
-609938427L // the length of the file in bytes
+"6", // the version of the APK that the file was uploaded against
+618717681L // the length of the file in bytes
 )
 };
 };
