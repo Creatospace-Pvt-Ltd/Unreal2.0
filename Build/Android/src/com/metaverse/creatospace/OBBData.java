67d378f0bb7122eda7eb2267848e3479d30c42b7
Saswat Mishra <saswatmishrabablu@gmail.com>
2024-03-22 04:28:26 +0530

    Screen share yet to be finished

--- Build/Android/src/com/metaverse/creatospace/OBBData.java
+++ Build/Android/src/com/metaverse/creatospace/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"12", // the version of the APK that the file was uploaded against
-491526899L // the length of the file in bytes
+"15", // the version of the APK that the file was uploaded against
+615792236L // the length of the file in bytes
 )
 };
 };
